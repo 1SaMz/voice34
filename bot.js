c7340edeae218b677591193fb42139e08a538a82
1SaMz <44713603+1SaMz@users.noreply.github.com>
2019-03-22 16:45:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,36 +31,3 @@ client.on('message', message => {
  
   });
 client.login(process.env.BOT_TOKEN);
-
-
-
-
-
-const client2 = new Discord.Client2();
- 
-const devs = ["558359137864515584"]
- 
-const adminprefix = "#";
-client2.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-
-     
-
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client2.user.setGame(argresult, "https://www.twitch.tv/Mrx-Dev");
-      message.channel.sendMessage(`** - # تـم تـحويـل إلـى وضعيـهة ، Streaming . :lollipop:  ${argresult}**`)
-  }
-    if(message.content === adminprefix + "restart") {
-      if (!devs.includes(message.author.id)) return;
-          message.channel.send(`:warning:️ ** restarting by ${message.author.username}**`);
-        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        console.log(` Bot restarting...Maaaz `);
-        console.log("===============================================\n\n");
-        client2.destroy();
-        child_process.fork(__dirname + "/bot.js");
-        console.log(`Bot Successfully Restarted`);
-    }
- 
-  });
-client2.login(process.env.BOT_TOKEN2);
